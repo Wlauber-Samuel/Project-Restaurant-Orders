ff91c5c41a9ecb1d68b3e7b382e95c82a5798338
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-05 19:18:35 -0300

    feat: implementa a classe menu_data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,42 @@
-# Req 3
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+from src.models.ingredient import Restriction
+
+import sys
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        file = self.read_file(source_path)
+        self.add_ingredients_for_dishes(file)
+
+    def read_file(self, path_file):
+        if not path_file.endswith('.csv'):
+            sys.stderr.write('Formato inválido\n')
+            return None
+        try:
+            with open(path_file, 'r', encoding='utf-8') as file:
+                file_lines = file.read().split("\n")
+                return file_lines
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+            return None
+
+    def add_ingredients_for_dishes(self, file):
+        dishes = {}
+
+        for index in range(1, len(file) - 1):
+            dish_information = file[index].split(",")
+
+            if dish_information[0] not in dishes:
+                dishes[dish_information[0]] = Dish(
+                    dish_information[0], float(dish_information[1]))
+
+            new_ingredient = Ingredient(dish_information[2])
+            dishes[dish_information[0]].add_ingredient_dependency(
+                new_ingredient, int(dish_information[3]))
+
+        if (file[len(file) - 2]):
+            for dish in dishes.values():
+                self.dishes.add(dish)
---
+++ src/test01.py
@@ -0,0 +1,3 @@
+from src.services.menu_data import MenuData
+
+obj = MenuData('tests\mocks\menu_base_data.csv')
