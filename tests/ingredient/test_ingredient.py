77ac1963915da0b163275fc46fd98b349564b9c8
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-05 15:07:01 -0300

    feat: implementa os primeiros asserts do teste da classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,24 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_butter = Ingredient("manteiga")
+    ingredient_butter02 = Ingredient("manteiga")
+    ingredient_shrimp = Ingredient("camarão")
+
+    assert ingredient_butter.name == "manteiga"
+    assert ingredient_butter.restrictions == {"LACTOSE", "ANIMAL_DERIVED"}
+    assert ingredient_shrimp.name == "camarão"
+    assert ingredient_shrimp.restrictions == {
+        "ANIMAL_MEAT", "ANIMAL_DERIVED", "SEAFOOD"}
+    assert ingredient_butter02.name == "manteiga"
+    assert ingredient_butter02.restrictions == {"LACTOSE", "ANIMAL_DERIVED"}
+
+    assert type(ingredient_butter.__hash__()) == int
+    assert type(ingredient_shrimp.__hash__()) == int
+    assert ingredient_butter.__hash__() == ingredient_butter02.__hash__()
+    assert ingredient_butter.__hash__() != ingredient_shrimp.__hash__()
+    assert ingredient_butter.__eq__(ingredient_butter02) == True
+    assert ingredient_butter.__eq__(ingredient_shrimp) == False
+    assert ingredient_shrimp.__eq__(ingredient_butter) == False
+    assert ingredient_butter.__repr__() == "ingredient(manteiga)"
+    assert ingredient_shrimp.__repr__() == "ingredient(camarão)"
